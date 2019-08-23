7d1515b78cfd54fdd0e64a3937e7a6200d4dc1ba
Scott Arenz <scottarenz@gmail.com>
2019-08-23 00:55:11 -0400

    Comment out FB app

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,17 +35,17 @@ module.exports = {
   siteMetadata: {
     title: config.siteTitle,
     description: config.siteDescription,
-    siteUrl: config.siteUrl,
-    algolia: {
-      appId: process.env.ALGOLIA_APP_ID ? process.env.ALGOLIA_APP_ID : "",
-      searchOnlyApiKey: process.env.ALGOLIA_SEARCH_ONLY_API_KEY
-        ? process.env.ALGOLIA_SEARCH_ONLY_API_KEY
-        : "",
-      indexName: process.env.ALGOLIA_INDEX_NAME ? process.env.ALGOLIA_INDEX_NAME : ""
-    },
-    facebook: {
-      appId: process.env.FB_APP_ID ? process.env.FB_APP_ID : ""
-    }
+    siteUrl: config.siteUrl
+    // algolia: {
+    //   appId: process.env.ALGOLIA_APP_ID ? process.env.ALGOLIA_APP_ID : "",
+    //   searchOnlyApiKey: process.env.ALGOLIA_SEARCH_ONLY_API_KEY
+    //     ? process.env.ALGOLIA_SEARCH_ONLY_API_KEY
+    //     : "",
+    //   indexName: process.env.ALGOLIA_INDEX_NAME ? process.env.ALGOLIA_INDEX_NAME : ""
+    // },
+    // facebook: {
+    //   appId: process.env.FB_APP_ID ? process.env.FB_APP_ID : ""
+    // }
   },
   plugins: [
     `gatsby-plugin-styled-jsx`, // the plugin's code is inserted directly to gatsby-node.js and gatsby-ssr.js files
